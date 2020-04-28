aef3ee7a6fdbaca0fef1f527f63b0e0a4f75a182
Live session user <roi2307@gmail.com>
2020-04-28 21:40:10 +0300

    place holder for tests

---
+++ Test.cpp
@@ -0,0 +1,17 @@
+/**
+ * unit test to solver
+ * AUTHOR: RoiPeleg TalAbed
+ * */
+#include "doctest.h"
+#include "solver.hpp"
+using namespace solver;
+
+#include <string>
+using namespace std;
+
+TEST_CASE("OP*"){
+
+}
+TEST_CASE("OP+"){
+    
+}
